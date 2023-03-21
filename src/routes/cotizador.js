9b5ce0856f311ef26152036690cb50ee2bb413bb
Juan Pablo <jbernal@intelecto.co>
2023-03-20 23:01:42 -0500

    Se agregan nuevas rutas para traer data parametrica

--- src/routes/cotizador.js
+++ src/routes/cotizador.js
@@ -1,17 +1,17 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 const jwt = require('jsonwebtoken');
 
 const Material = require('../models/Material');
 
-router.post('/crearMaterial', async (req,res) => {
-    const {nombreMaterial,material,largo_m,ancho_m,grm_m2,costo_sinIva_Rollo} = req.body;
-    const newTask = new Material({ nombreMaterial,material,largo_m,ancho_m,grm_m2,costo_sinIva_Rollo});
+router.post('/crearMaterial', async (req, res) => {
+    const { nombreMaterial, material, largo_m, ancho_m, grm_m2, costo_sinIva_Rollo } = req.body;
+    const newTask = new Material({ nombreMaterial, material, largo_m, ancho_m, grm_m2, costo_sinIva_Rollo });
     await newTask.save();
 
     res.status(200).json(newTask)
 });
-router.get('/AllMateriales',(req,res) =>{
+router.get('/AllMateriales', (req, res) => {
 
     Material.find().then((result) => {
         res.send(result)
@@ -19,55 +19,64 @@ router.get('/AllMateriales',(req,res) =>{
         console.log(err)
     });
 });
-router.post('/MaterialById', (req,res) => {
-    console.log(req.body);
-    const {nombreMaterial} = req.body;
-    console.log(nombreMaterial);
+router.post('/MaterialById', (req, res) => {
+    const { nombreMaterial } = req.body;
 
-    Material.findOne({nombreMaterial:nombreMaterial}).then((result) =>{
+    Material.findOne({ nombreMaterial: nombreMaterial }).then((result) => {
         res.status(200).json(result)
-    }).catch((err) =>{
+    }).catch((err) => {
         console.log(err)
     });
-
-
 });
-router.put('/actualizarMaterial', (req,res) => {
+router.put('/actualizarMaterial', (req, res) => {
 
-    const {_id,nombreMaterial,material,largo_m,ancho_m,grm_m2,costo_sinIva_Rollo} = req.body;
+    const { _id, nombreMaterial, material, largo_m, ancho_m, grm_m2, costo_sinIva_Rollo } = req.body;
 
     Material.updateOne(
         { _id: _id },
-        { $set: { nombreMaterial: nombreMaterial,
-            material:material,
-            largo_m:largo_m,
-            ancho_m:ancho_m,
-            grm_m2:grm_m2,
-            costo_sinIva_Rollo:costo_sinIva_Rollo
-        } },
+        {
+            $set: {
+                nombreMaterial: nombreMaterial,
+                material: material,
+                largo_m: largo_m,
+                ancho_m: ancho_m,
+                grm_m2: grm_m2,
+                costo_sinIva_Rollo: costo_sinIva_Rollo
+            }
+        },
         (err, result) => {
-          if (err) throw err;
-          console.log(`${result.modifiedCount} registro(s) actualizado(s)`);
-          res.status(201).json(`${result.modifiedCount} registro(s) actualizado(s)`);
+            if (err) throw err;
+            console.log(`${result.modifiedCount} registro(s) actualizado(s)`);
+            res.status(201).json(`${result.modifiedCount} registro(s) actualizado(s)`);
         }
-      );
+    );
 
 });
 
 const Impresion = require('../models/Impresion');
 //#region Impresion 
-router.post('/crearImpresion', async (req,res) => {
-    const {nombreImpresion,costoImpresion} = req.body;
-    const newImpresion = new Impresion({ nombreImpresion,costoImpresion});
+router.post('/crearImpresion', async (req, res) => {
+    const { nombreImpresion, costoImpresion } = req.body;
+    const newImpresion = new Impresion({ nombreImpresion, costoImpresion });
     await newImpresion.save();
 
     res.status(200).json(newImpresion)
 });
 
-router.get('/AllImpresiones',(req,res) =>{
+router.get('/AllImpresiones', (req, res) => {
 
     Impresion.find().then((result) => {
         res.send(result)
+    }).catch((err) => {
+        console.log(err)
+    });
+});
+
+router.post('/ImpresionById', (req, res) => {
+    const { nombreImpresion } = req.body;
+
+    Impresion.findOne({ nombreImpresion: nombreImpresion }).then((result) => {
+        res.status(200).json(result)
     }).catch((err) => {
         console.log(err)
     });
@@ -79,18 +88,30 @@ const Confeccion = require('../models/Confeccion');
 
 //#region Confeccion
 
-router.post('/crearConfeccion', async (req,res) => {
-    const {nombreConfeccion,costoConfeccion} = req.body;
-    const newConfeccion = new Confeccion({ nombreConfeccion,costoConfeccion});
+router.post('/crearConfeccion', async (req, res) => {
+    const { nombreConfeccion, costoConfeccion } = req.body;
+    const newConfeccion = new Confeccion({ nombreConfeccion, costoConfeccion });
     await newConfeccion.save();
 
     res.status(200).json(newConfeccion)
 });
 
-router.get('/AllConfecciones',(req,res) =>{
+router.get('/AllConfecciones', (req, res) => {
 
     Confeccion.find().then((result) => {
         res.send(result)
+    }).catch((err) => {
+        console.log(err)
+    });
+});
+
+
+router.post('/ConfeccionById', (req, res) => {
+    
+    const { nombreConfeccion } = req.body;
+
+    Confeccion.findOne({ nombreConfeccion: nombreConfeccion }).then((result) => {
+        res.status(200).json(result)
     }).catch((err) => {
         console.log(err)
     });
@@ -101,15 +122,15 @@ router.get('/AllConfecciones',(req,res) =>{
 const Cordon = require('../models/Cordon');
 //#region Cordon
 
-router.post('/crearCordon', async (req,res) => {
-    const {nombreCordon,largoRollo,valorRollo,valorMetro} = req.body;
-    const newCordon = new Cordon({ nombreCordon,largoRollo,valorRollo,valorMetro});
+router.post('/crearCordon', async (req, res) => {
+    const { nombreCordon, largoRollo, valorRollo, valorMetro } = req.body;
+    const newCordon = new Cordon({ nombreCordon, largoRollo, valorRollo, valorMetro });
     await newCordon.save();
 
     res.status(200).json(newCordon)
 });
 
-router.get('/AllCordones',(req,res) =>{
+router.get('/AllCordones', (req, res) => {
 
     Cordon.find().then((result) => {
         res.send(result)
@@ -119,6 +140,40 @@ router.get('/AllCordones',(req,res) =>{
 });
 
 //#endregion Cordon
+
+
+
+//#region Cogedera
+const Cogedera = require('../models/Cogedera');
+
+router.post('/crearCogedera', async (req, res) => {
+    const { nombreCogedera, costoCogedera } = req.body;
+    const newCogedera = new Cogedera({ nombreCogedera, costoCogedera });
+    await newCogedera.save();
+
+    res.status(200).json(newCogedera)
+});
+
+router.get('/AllCogederas', (req, res) => {
+
+    Cogedera.find().then((result) => {
+        res.send(result)
+    }).catch((err) => {
+        console.log(err)
+    });
+});
+
+router.post('/CogederaById', (req, res) => {
+    const { nombreCogedera } = req.body;
+
+    Cogedera.findOne({ nombreCogedera: nombreCogedera }).then((result) => {
+        res.status(200).json(result)
+    }).catch((err) => {
+        console.log(err)
+    });
+});
+
+//#endregion Cogedera
 
 
 
