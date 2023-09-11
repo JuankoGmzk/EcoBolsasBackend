640a4e17133d7fb19195ba9c85a4f2fa60319578
JuankoGmzk <juanpis0304@hotmail.com>
2023-09-11 17:24:07 -0500

    Se ajusta el método que crea el usuario, agregando más parametros. - Se hace una mejor identación al código.

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,24 +13,41 @@ router.get('/index', (req, res) => {
 });
 
 router.post('/signup', async (req, res) => {
-    const { email, password } = req.body;
-    console.log(email," and ",password)
-    const newUser = new User({email, password});
+    const { primerNombre, segundoNombre, primerApellido, segundoApellido, cedula, cargo, edad, fechaNacimiento, esPLanta, genero, user, password } = req.body;
+    console.log(user, " and ", password)
+    const newUser = new User({ primerNombre, segundoNombre, primerApellido, segundoApellido, cedula, cargo, edad, fechaNacimiento, esPLanta, genero, user, password });
     await newUser.save();
-		const token = await jwt.sign({_id: newUser._id}, 'secretkey');
-    res.status(200).json({token});
+    const token = await jwt.sign({ _id: newUser._id }, 'secretkey');
+    res.status(200).json({ token });
 });
 
 router.post('/signin', async (req, res) => {
     const { email, password } = req.body;
 
-    const user = await User.findOne({email});
-    if (!user) return res.status(401).send('The email doen\' exists');
-    if (user.password !== password) return res.status(401).send('Wrong Password');
+    console.log(req.body);
 
-		const token = jwt.sign({_id: user._id}, 'secretkey');
+    const criterioBusqueda = { user:email}; 
 
-    return res.status(200).json({token});
+    const userFind = await User.find(criterioBusqueda);
+    if (userFind.length == 0) return res.status(401).send('The user doen\' exists');
+   
+    if (userFind[0].password !== password) return res.status(401).send('Contraseña incorrecta');
+   
+    const token = jwt.sign({ _id: userFind._id }, 'secretkey');
+    console.log(userFind[0]._id)
+    const dataUser = { 
+        primerNombre:userFind[0].primerNombre,
+        segundoNombre:userFind[0].segundoNombre,
+        primerApellido:userFind[0].primerApellido,
+        cedula:userFind[0].cedula,
+        edad:userFind[0].edad,
+        cargo:userFind[0].cargo,
+        genero:userFind[0].genero,
+        user:userFind[0].user,
+        idUser:userFind[0]._id
+    };
+
+    return res.status(200).json({ token, dataUser });
 });
 
 router.get('/tasks', (req, res) => {
@@ -43,29 +60,31 @@ router.get('/tasks', (req, res) => {
 });
 
 router.post('/createTask', async (req, res) => {
-    const {empresa,fecha_entrega,fecha,
-    supervisor,material,tipo_bolsa,
-    cogedera,color_tela,medidas,
-    marcacion,color_tintas,
-    cantidad,name, description, date} = req.body;
-    const newTask = new Task({ empresa,fecha_entrega,fecha,
-        supervisor,material,tipo_bolsa,
-        cogedera,color_tela,medidas,
-        marcacion,color_tintas,
-        cantidad,name, description, date});
+    const { empresa, fecha_entrega, fecha,
+        supervisor, material, tipo_bolsa,
+        cogedera, color_tela, medidas,
+        marcacion, color_tintas,
+        cantidad, name, description, date } = req.body;
+    const newTask = new Task({
+        empresa, fecha_entrega, fecha,
+        supervisor, material, tipo_bolsa,
+        cogedera, color_tela, medidas,
+        marcacion, color_tintas,
+        cantidad, name, description, date
+    });
     await newTask.save();
     res.status(200).json(newTask);
 
 });
 
-router.delete('/deleteTaks',(req, res) => {
+router.delete('/deleteTaks', (req, res) => {
     const deleteTask = Task.deleteMany().then((result) => {
         res.status(201).json(deleteTask);
     }).catch((err) => {
         console.log(err)
     });
-    
-   
+
+
 });
 
 router.post('/deleteTaksByid', (req, res) => {
@@ -82,22 +101,22 @@ router.post('/deleteTaksByid', (req, res) => {
 });
 
 
-router.put('/updateTask',verifyToken,(req,res) => {
+router.put('/updateTask', verifyToken, (req, res) => {
 
     const { _id } = req.body;
     Task.updateOne(
         { _id: _id },
-        { $set: { cantidad: 1502} },
+        { $set: { cantidad: 1502 } },
         (err, result) => {
-          if (err) throw err;
-          console.log(`${result.modifiedCount} registro(s) actualizado(s)`);
-          res.status(201).json(`${result.modifiedCount} registro(s) actualizado(s)`);
+            if (err) throw err;
+            console.log(`${result.modifiedCount} registro(s) actualizado(s)`);
+            res.status(201).json(`${result.modifiedCount} registro(s) actualizado(s)`);
         }
-      );
+    );
 });
 
 router.get('/private-tasks', verifyToken, (req, res) => {
-    
+
     PrivateTask.find().then((result) => {
         res.send(result)
     }).catch((err) => {
@@ -105,18 +124,18 @@ router.get('/private-tasks', verifyToken, (req, res) => {
     });
 });
 
-router.post('/createPrivateTask',verifyToken, async (req,res) =>{
-    
-    const {name, description, date} = req.body;
-    
-    const newPrivateTask = new PrivateTask({name, description, date});
+router.post('/createPrivateTask', verifyToken, async (req, res) => {
+
+    const { name, description, date } = req.body;
+
+    const newPrivateTask = new PrivateTask({ name, description, date });
 
     await newPrivateTask.save();
     res.status(200).json(newPrivateTask);
 
 });
 
-router.delete('/deleteAllPrivateTask', verifyToken, (req, res) =>{
+router.delete('/deleteAllPrivateTask', verifyToken, (req, res) => {
     const deletePrivateTask = PrivateTask.deleteMany().then((result) => {
         res.status(201).json(deletePrivateTask);
     }).catch((err) => {
@@ -125,25 +144,25 @@ router.delete('/deleteAllPrivateTask', verifyToken, (req, res) =>{
 });
 
 async function verifyToken(req, res, next) {
-	try {
-		if (!req.headers.authorization) {
-			return res.status(401).send('Unauhtorized Request');
-		}
-		let token = req.headers.authorization.split(' ')[1];
-		if (token === 'null') {
-			return res.status(401).send('Unauhtorized Request');
-		}
+    try {
+        if (!req.headers.authorization) {
+            return res.status(401).send('Unauhtorized Request');
+        }
+        let token = req.headers.authorization.split(' ')[1];
+        if (token === 'null') {
+            return res.status(401).send('Unauhtorized Request');
+        }
 
-		const payload = await jwt.verify(token, 'secretkey');
-		if (!payload) {
-			return res.status(401).send('Unauhtorized Request');
-		}
-		req.userId = payload._id;
-		next();
-	} catch(e) {
-		//console.log(e)
-		return res.status(401).send('Unauhtorized Request');
-	}
+        const payload = await jwt.verify(token, 'secretkey');
+        if (!payload) {
+            return res.status(401).send('Unauhtorized Request');
+        }
+        req.userId = payload._id;
+        next();
+    } catch (e) {
+        //console.log(e)
+        return res.status(401).send('Unauhtorized Request');
+    }
 }
 
 module.exports = router;
