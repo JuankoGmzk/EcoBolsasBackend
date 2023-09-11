d16bfaeaaae155416bebd805ad5183e8b2804ee2
JuankoGmzk <juanpis0304@hotmail.com>
2023-09-11 17:23:07 -0500

    - Se agrega un nuevo método post para crear cotización. - Se ajusta el método de crearMaterial para poder hacerlo masivo. - Se agrega un nuevo método de cotizar que se encarga de la lógica fuerte del cotizador

--- src/routes/cotizador.js
+++ src/routes/cotizador.js
@@ -3,14 +3,81 @@ const router = Router();
 const jwt = require('jsonwebtoken');
 
 const Material = require('../models/Material');
+const Cotizador = require('../models/Cotizaciones');
+
+
+router.post('/CrearCotizacion', async (req, res) => {
+
+
+    try {
+
+        const { NombreEmpresa, NombreContacto, Identificacion, TelContacto,
+            Email, FechaEntrega, Ciudad, DireccionEntrega,
+            TipoCogedera, TipoBolsa, UnidadesRequeridas, Ancho_cm,
+            Alto_cm, Fuelle_cm, Asas_cm, Material, Color, Estampado,
+            Caras, NumeroTintas, ColorTintas, ValorBolsa, Utilidad, PVSinIvaUnitario, PVSinIvaTotal,
+            PVConIvaUnitario, PVConIvaTotal, NombreUsuario, _IdUsuario, _EstadoCotizacion, CheckCliente, CheckDineroCliente
+        } = req.body;
+
+        console.log("CrearCotizacion", req.body)
+
+        const newCotizacion = new Cotizaciones({
+            NombreEmpresa, NombreContacto, Identificacion, TelContacto,
+            Email, FechaEntrega, Ciudad, DireccionEntrega,
+            TipoCogedera, TipoBolsa, UnidadesRequeridas, Ancho_cm,
+            Alto_cm, Fuelle_cm, Asas_cm, Material, Color, Estampado,
+            Caras, NumeroTintas, ColorTintas, ValorBolsa, Utilidad, PVSinIvaUnitario, PVSinIvaTotal,
+            PVConIvaUnitario, PVConIvaTotal, NombreUsuario, _IdUsuario, _EstadoCotizacion, CheckCliente, CheckDineroCliente
+        });
+
+        await newCotizacion.save();
+
+        const objResp = {
+            respCotizacion: newCotizacion,
+            resp: true,
+            message: 'Creación de cotización con éxito'
+        }
+
+        return res.status(200).json(objResp);
+    }
+    catch (error) {
+        
+        const objResp = {
+            respCotizacion: null,
+            resp: false,
+            message: error.message
+        }
+
+        return res.status(400).json(objResp);
+    }
+
+
+   
+
+});
 
 router.post('/crearMaterial', async (req, res) => {
-    const { nombreMaterial, material, largo_m, ancho_m, grm_m2, costo_sinIva_Rollo } = req.body;
-    const newTask = new Material({ nombreMaterial, material, largo_m, ancho_m, grm_m2, costo_sinIva_Rollo });
-    await newTask.save();
 
-    res.status(200).json(newTask)
+    for (let i = 0; i < req.body.length; i++) {
+
+        const elemento = req.body[i];
+
+        nombreMaterial = elemento.nombreMaterial;
+        material = elemento.material;
+        largo_m = elemento.largo_m;
+        ancho_m = elemento.ancho_m;
+        grm_m2 = elemento.grm_m2;
+        costo_sinIva_Rollo = elemento.costo_sinIva_Rollo;
+
+        const newTask = new Material({ nombreMaterial, material, largo_m, ancho_m, grm_m2, costo_sinIva_Rollo });
+
+        await newTask.save();
+
+    }
+
+    return res.status(200).json({ message: req.body.length + ' Datos guardados exitosamente' });
 });
+
 router.get('/AllMateriales', (req, res) => {
 
     Material.find().then((result) => {
@@ -107,7 +174,7 @@ router.get('/AllConfecciones', (req, res) => {
 
 
 router.post('/ConfeccionById', (req, res) => {
-    
+
     const { nombreConfeccion } = req.body;
 
     Confeccion.findOne({ nombreConfeccion: nombreConfeccion }).then((result) => {
@@ -141,10 +208,9 @@ router.get('/AllCordones', (req, res) => {
 
 //#endregion Cordon
 
-
-
 //#region Cogedera
 const Cogedera = require('../models/Cogedera');
+const Cotizaciones = require('../models/Cotizaciones');
 
 router.post('/crearCogedera', async (req, res) => {
     const { nombreCogedera, costoCogedera } = req.body;
@@ -176,5 +242,212 @@ router.post('/CogederaById', (req, res) => {
 //#endregion Cogedera
 
 
+//#region Cotizador
+router.post('/Cotizar', async (req, res) => {
+
+    const MaterialMongoose = require('../models/Material');
+    const ImpresionMongoose = require('../models/Impresion');
+    const ConfeccionMongoose = require('../models/Confeccion');
+    const CogederaMongoose = require('../models/Cogedera');
+
+    const { TipoCogedera, TipoBolsa, UnidadesRequeridas, Ancho, Alto, Fuelle, Asas, Material, Color, Estampado, Caras, NumeroTintas, ColorTintas } = req.body;
+    const consultaMaterial = await MaterialMongoose.findById(Material);
+
+    const consultaImpresion = await ImpresionMongoose.findById(Estampado);
+
+    const consultaConfeccion = await ConfeccionMongoose.findById(TipoBolsa);
+
+    const cogederaTroquelid = '64f9085a3cc1ec0dec57252d';
+    const consultaCogedera = await CogederaMongoose.findById(cogederaTroquelid);
+
+    if (consultaMaterial == null) {
+        console.log("acá devemos devoler el error ")
+    }
+
+    if (consultaImpresion == null) {
+        console.log("acá devemos devoler el error ")
+    }
+
+    if (consultaConfeccion == null) {
+        console.log("acá devemos devoler el error ")
+    }
+
+    if (consultaCogedera == null) {
+        console.log("acá devemos devoler el error ")
+    }
+
+    consultaMaterial.ancho_m = consultaMaterial.ancho_m * 100;
+    let ValorAsas = 0;
+    let SobranteFuelleCompletoH = 0;
+    let SobranteFuelleCompletoV = 0;
+
+    let sumaPorcentaje = (parseInt(UnidadesRequeridas) * 2) / 100;
+    let TotalUnidades = parseInt(UnidadesRequeridas) + sumaPorcentaje;
+
+    let CortesPorRollo = 0;
+    let RollosRequeridos = 0;
+    //Calculo Bolsa
+    let TamañoCorteBolsa = 0;
+    const ValorHolguraAlto = TipoCogedera == 'Troquel' ? 8 : 5;
+
+    let DimensionesCorteBolsaAncho = parseInt(Ancho) + 2 + 0;
+    let DimensionesCorteBolsaAlto = (parseInt(Alto) * 2) + parseInt(ValorHolguraAlto) + parseInt(Fuelle);
+
+    //#region Calculo de cortes H|V
+
+    //CorteHorizontal
+    let CorteAnchoRolloH = Math.floor(consultaMaterial.ancho_m / DimensionesCorteBolsaAlto);
+    let UsoMaterialH = CorteAnchoRolloH * DimensionesCorteBolsaAlto;
+    let SobranteH = consultaMaterial.ancho_m - UsoMaterialH;
+    let CortesRequeridosH = Math.ceil(TotalUnidades / CorteAnchoRolloH)
+    let MetrosRequeridosH = Math.ceil((CortesRequeridosH * DimensionesCorteBolsaAncho) / 100)
+
+    //CorteVertical 
+
+    let CorteAnchoRolloV = Math.floor(consultaMaterial.ancho_m / DimensionesCorteBolsaAncho);
+    let UsoMaterialV = CorteAnchoRolloV * DimensionesCorteBolsaAncho;
+    let SobranteV = consultaMaterial.ancho_m - UsoMaterialV;
+    let CortesRequeridosV = Math.ceil(TotalUnidades / CorteAnchoRolloV)
+    let MetrosRequeridosV = Math.ceil((CortesRequeridosV * DimensionesCorteBolsaAlto) / 100)
+
+
+    //#endregion Calculo de cortes H|V
+
+    if(Fuelle.length != 0){
+        SobranteFuelleCompletoH= CalcularSobranteFuelleComleto(MetrosRequeridosH, Fuelle, Alto, SobranteH, TotalUnidades)
+        SobranteFuelleCompletoV = CalcularSobranteFuelleComleto(MetrosRequeridosV, Fuelle, Alto, SobranteV, TotalUnidades, true)
+    }
+
+    if (Asas.length != 0) {
+        ValorAsas = CalcularAsas(Asas, TotalUnidades, consultaMaterial.ancho_m)
+    }
+
+    const ValorMetrosH = MetrosRequeridosH + SobranteFuelleCompletoH + ValorAsas;
+    const ValorMetrosV = MetrosRequeridosV + SobranteFuelleCompletoV + ValorAsas;
+    const MinimoValorMetros = ValorMetrosH < ValorMetrosV ? ValorMetrosH : ValorMetrosV;
+    const RutaImagen = ValorMetrosH < ValorMetrosV ? 'rolloHorizontal' : 'rolloVertical';
+
+
+    CortesPorRollo = MetrosRequeridosH;
+    RollosRequeridos = redondearAlDecimo(MinimoValorMetros/parseInt(consultaMaterial.largo_m))
+
+    //#region  ValoresFinales
+
+    const ValorSubtotalMaterial = Math.ceil(parseInt(consultaMaterial.costo_sinIva_Rollo)*RollosRequeridos);
+    const IvaMaterial = Math.ceil(ValorSubtotalMaterial*0.19);
+    const ValorTransporte = 50000;
+    const ValorFinalMaterial = parseInt(ValorSubtotalMaterial+IvaMaterial+ValorTransporte);
+    
+
+    const ValorFinalConfeccion = parseInt(consultaConfeccion.costoConfeccion)*TotalUnidades;
+    const ValorFinalCorte = 50000*RollosRequeridos;
+    const TotalResumenCostos = ValorFinalMaterial+ValorFinalConfeccion+ValorFinalCorte;
+    const ValorPorBolsa = Math.ceil(TotalResumenCostos/parseInt(UnidadesRequeridas));
+
+    const objResumenCostos = {
+        PTMaterial : ValorFinalMaterial,
+        PTConfeccion : ValorFinalConfeccion,
+        PTCorte : ValorFinalCorte,
+        PTImpresion : null,
+        PTAccesorios : null,
+        PTOtros : null,
+        SumaTotalCostos : TotalResumenCostos,
+        ValorPorBolsa : ValorPorBolsa
+    }
+
+    //#endregion ValoresFinales
+
+    //#region  PrecioVenta
+
+    let Ajuste = 20;
+    let AjusteSumaPorcentaje = (parseInt(ValorPorBolsa) * Ajuste) / 100;
+    const PrecioVentaUnitarioSinIVA =   Math.ceil(ValorPorBolsa+AjusteSumaPorcentaje);
+    const PrecioVentaTotalSinIVA = PrecioVentaUnitarioSinIVA*parseInt(UnidadesRequeridas);
+ 
+    const PrecioVentaUnitariaMasIVA = Math.ceil(PrecioVentaUnitarioSinIVA*0.19);
+    const PrecioVentaTotalMasIVA = Math.ceil(PrecioVentaTotalSinIVA*0.19);
+
+    const TotalVentaBolsaUnitaria =  Math.ceil(PrecioVentaUnitarioSinIVA+PrecioVentaUnitariaMasIVA);
+    const TotalVentaBolsas = Math.ceil(PrecioVentaTotalSinIVA+PrecioVentaTotalMasIVA);
+
+
+    const objPrecioVenta = {
+        PVSinIvaUnitario : PrecioVentaUnitarioSinIVA,
+        PVSinIvaTotal : PrecioVentaTotalSinIVA,
+        PVConIvaUnitario : TotalVentaBolsaUnitaria,
+        PVConIvaTotal : TotalVentaBolsas,
+        IVA19Unitario : PrecioVentaUnitariaMasIVA,
+        IVA19Total : PrecioVentaTotalMasIVA
+    }
+
+    const objResp = {
+        message : "Datos guardados exitosamente",
+        resp : true,
+        rutaImagen : RutaImagen,
+        PreciosVenta : objPrecioVenta,
+        ResumenCosto : objResumenCostos
+    };
+
+    //#endregion PrecioVenta
+
+    return res.status(200).json(objResp);
+});
+
+//MetrosRequeridosAncho:MR
+//Alto Fuelle : AF
+//Alto Bolsa : AB
+//Sobrante : S
+//Metros Requeridos Horizontal:MRH
+
+function CalcularSobranteFuelleComleto(strMR, strAF, strAB, strS, intTotalUnidades, isVertical = false) {
+
+    let sobranteV = Math.floor(Number(strS) / Number(strAF)) * Math.floor((Number(strMR) * 100) / Number(strAB));
+    let sobranteH = Math.floor(Number(strS) / Number(strAB)) * Math.floor((Number(strMR) * 100) / Number(strAF));
+
+    let UnidadesRequeridas = intTotalUnidades * 2 - Math.max(sobranteV, sobranteH);
+    if (UnidadesRequeridas < 0) {
+        return 0;
+    }
+
+    let CortesxAR;
+    let UsoMaterial;
+    let Desperdicio;
+    let CortesRequeridos;
+    let MetrosRequeridos;
+
+    if (isVertical) {
+        CortesxAR = Math.floor(Number(strMR) / Number(strAF));
+        UsoMaterial = CortesxAR * Number(strAF);
+        Desperdicio = Number(strMR) - UsoMaterial;
+        CortesRequeridos = Math.ceil(UnidadesRequeridas / CortesxAR);
+        MetrosRequeridos = Math.ceil((CortesRequeridos * Number(strAB)) / 100);
+    }
+    else {
+        CortesxAR = Math.floor(Number(strMR) / Number(strAB));
+        UsoMaterial = CortesxAR * Number(strAB);
+        Desperdicio = Number(strMR) - UsoMaterial;
+        CortesRequeridos = Math.ceil(UnidadesRequeridas / CortesxAR);
+        MetrosRequeridos = Math.ceil((CortesRequeridos * Number(strAF)) / 100);
+    }
+
+    return MetrosRequeridos;
+}
+
+
+function CalcularAsas(strLA, strTU, strAR) {
+    const AnchoAsas = 6;
+    const LonAsasBolsa = parseInt(strLA) * 2;
+    const LargoTotal = (LonAsasBolsa * parseInt(strTU) / 100)
+    const CortesAnchoRollo = Math.floor(parseInt(strAR) / AnchoAsas);
+    const MetrosAsas = Math.ceil(LargoTotal / CortesAnchoRollo);
+
+    return MetrosAsas;
+}
+
+function redondearAlDecimo(numero) {
+    return Math.ceil(numero * 10) / 10;
+  }
+
+//#endregion Cotizador
 
 module.exports = router;
