fe06d01ce9517ed06245a61415ae2c6cab921aee
JuankoGmzk <juanpis0304@hotmail.com>
2023-09-11 17:21:49 -0500

    Se agrega un nuevo modelo de en la base de datos para los usuarios

--- src/models/User.js
+++ src/models/User.js
@@ -1,9 +1,17 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema({
-    email: String,
-    password: String,
-    rol: String
+    primerNombre: String,
+    segundoNombre: String,
+    primerApellido: String,
+    segundoApellidoegundoApellido: String,
+    cedula: String,
+    edad: String,
+    cargo: String,
+    esPlanta: String,
+    genero : String,
+    user: String,
+    password : String
 }, {
     timestamps: true
 });
