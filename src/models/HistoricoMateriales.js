57d66236f4e976553c3d34fd457c0f0020fdaf1d
JuankoGmzk <juanpis0304@hotmail.com>
2023-09-19 21:11:10 -0500

    Se crea el nuevo modelo de historico de materiales

---
+++ src/models/HistoricoMateriales.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+const HistoricoMaterialesSchema = new Schema({
+    _idMaterial: String,
+    costoAnterior:Number,
+    costoNuevo:Number,
+    nombreAsesorCambio: String
+}, {
+    timestamps: true
+});
+
+module.exports = model('historicoMateriales', HistoricoMaterialesSchema);
