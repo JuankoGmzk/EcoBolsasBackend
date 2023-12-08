631a3ac7d44916aa3501ef4dd4c2783df86a0f9b
juankogmzk <juanpis0304@hotmail.com>
2023-12-08 14:18:52 -0500

    se ajuste el mensaje de conexión y además se coloca otra url de conexión a la nube de azure cosmos

--- src/database.js
+++ src/database.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', true);
 
+
+//mongodb://127.0.0.1/EcoBolsasDev
+//mongoose.connect('mongodb://ecocosmos:mfK6LvEvG1AcI2qDPhFaMKV4kQtbcU6Ykg9In8zzpNYoiMbs90LkKs1WsUWW1UPgzijAJGFwbDIgACDbjIbdYg==@ecocosmos.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@ecocosmos@', {
 mongoose.connect('mongodb://127.0.0.1/EcoBolsasDev', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-    .then(db => console.log('Database is connected'))
-    .catch(err => console.log(err));
+}).then(db => {
+    console.log('Database is connected');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err.message);
+});
 
-    
+
