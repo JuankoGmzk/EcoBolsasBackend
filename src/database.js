0f75e031cb10037894b91351bacb9ec150e0fae8
juankogmzk <juanpis0304@hotmail.com>
2023-12-11 15:37:12 -0500

    se ajsuta la base de datos de producción

--- src/database.js
+++ src/database.js
@@ -4,8 +4,8 @@ mongoose.set('strictQuery', true);
 
 
 //mongodb://127.0.0.1/EcoBolsasDev
-//mongoose.connect('mongodb://ecocosmos:mfK6LvEvG1AcI2qDPhFaMKV4kQtbcU6Ykg9In8zzpNYoiMbs90LkKs1WsUWW1UPgzijAJGFwbDIgACDbjIbdYg==@ecocosmos.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@ecocosmos@', {
-mongoose.connect('mongodb://127.0.0.1/EcoBolsasDev', {
+mongoose.connect('mongodb://ecocosmos:mfK6LvEvG1AcI2qDPhFaMKV4kQtbcU6Ykg9In8zzpNYoiMbs90LkKs1WsUWW1UPgzijAJGFwbDIgACDbjIbdYg==@ecocosmos.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@ecocosmos@', {
+//mongoose.connect('mongodb://127.0.0.1/EcoBolsasDev', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(db => {
